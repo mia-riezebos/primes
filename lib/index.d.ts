35df50f8e80a39046dfb0f3919f3cb19ff6bc87d
mia-cx <mia@mia.cx>
2022-07-04 10:40:41 +0200

    build 0.0.1

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export declare const cache: Set<number>;
+/**
+ * generates an array of primes (within a given range)
+ * @param n amount of primes to generate OR start of range
+ * @param o end of range
+ * @returns array of prime numbers
+ */
+export default function generatePrimes(n: number, o?: number): number[];
